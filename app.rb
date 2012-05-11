2b74e6ac0e5b5053f9adb85191cb1d9e0b6814ed
César Salazar <i.am@cesarsalazar.mx>
2012-05-11 16:26:01 -0500

    /bancos

--- app.rb
+++ app.rb
@@ -34,7 +34,8 @@ get '/:url_to_redirect' do
   status 301
   urls = { "df"       => "http://cesarsalazar.pbworks.com/w/page/24857335/DF",
            "bio"      => "http://cesarsalazar.pbworks.com/w/page/52608776/Bio",
-           "cv"       => "http://cesarsalazar.pbworks.com/w/page/10755048/CV%20Espa%C3%B1ol" }
+           "cv"       => "http://cesarsalazar.pbworks.com/w/page/10755048/CV%20Espa%C3%B1ol",
+           "bancos"   => "http://cesarsalazar.pbworks.com/w/page/10755043/bancos" }
   urls.each do |url, location|
     redirect location if url == params[:url_to_redirect]
   end
